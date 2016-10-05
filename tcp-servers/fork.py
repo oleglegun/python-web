20f889528ce2109eeb9bfc97ddc348aa36343209
Oleg Legun <oleg.legun@gmail.com>
2016-10-05 15:06:55 +0300

    Add comments to fork.py

--- tcp-servers/fork.py
+++ tcp-servers/fork.py
@@ -10,18 +10,21 @@ server_socket.bind(('', 8080))
 server_socket.listen(10)
 
 while True:
-    client_socket, remote_address = server_socket.accept()
-    # When someone connects - OS gives us new File Descriptor for this connect.
+    client_socket, remote_address = server_socket.accept()  # Sleep and wait for new connect
+    # When someone connects - OS gives parent process new File Descriptor for this connect (socket)
+    # In this FD we can read/write to connected client
 
-    # Fork a child process. Return 0 in the child and the child's process id in the parent
-    child_pid = os.fork()
+    # We fork() w/ this new FD and continue working w/ it in child process
+    child_pid = os.fork()  # Fork a child process. Return 0 in the child and the child's process id in the parent
+
     if child_pid == 0:  # Means that child process was initiated and we now in child
         request = client_socket.recv(1024)
         client_socket.send(request.upper())
         print '(child {}) {} : {}'.format(client_socket.getpeername(), request)
-        client_socket.close()  # For OS GC
+        client_socket.close()  # Close child's FD for socket (for OS GC)
         sys.exit()  # kill child process
     else:  # Continues in parent process
-        client_socket.close()  # If we close socket only in child, it won't close in parent
+        client_socket.close()  # If we close socket FD only in child, it won't close in parent => memory leaks
+        # When there are no links to FD -> OS removes it from memory
 
 server_socket.close()
--- tcp-servers/simple.py
+++ tcp-servers/simple.py
@@ -4,12 +4,13 @@
 
 import socket
 
-server_socket = socket.socket()
+server_socket = socket.socket()  # Create socket() to listen for any connects
 server_socket.bind(('', 80))  # '' listen from all IPs, Default server ip: 127.0.0.1 80
-server_socket.listen(10)        # max connections queue: 10
+server_socket.listen(10)      # max connections queue: 10
 
 while True:
-    client_socket, remote_address = server_socket.accept()  # Accept a connection (wait in sleep for connection)
+    # Accept a connection (wait in sleep for connection)
+    client_socket, remote_address = server_socket.accept()  # Returns pointer to connection (FD), client IP address
     try:
         request = client_socket.recv(1024)  # Receive data from the socket (buffer = 1024 bytes)
         client_socket.send(request.upper())
